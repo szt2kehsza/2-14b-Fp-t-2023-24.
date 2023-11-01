fe57423d037fee5d0f73a15717b85b158f9ceaf7
szt2kehsza <115459926+szt2kehsza@users.noreply.github.com>
2023-11-01 09:32:24 +0100

    Orarend2 elkezdve

---
+++ Projektek/Projekt1/Orarend2.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menuIcon");
+    const menu = document.getElementById("menu");
+    const closeBtn = document.getElementById("closeBtn");
+    const cim = document.getElementById("cim");
+    const favcolorInput = document.getElementById("favcolor");
+    const fontStyleSelector = document.getElementById("font-style-selector");
+    const darkModeButton = document.getElementById("DarkMode");
+    const root = document.documentElement;
+
+    const data = {
+        szovegszine: "black",
+        fontstyle: "Arial",
+        darkMode: false,
+    };
+
+    function tartalomRendereles() {
+        root.style.setProperty("--szovegszine", data.szovegszine);
+        document.body.style.fontFamily = data.fontstyle;
+        const orakElemek = document.getElementsByClassName("orak");
+        for (let i = 0; i < orakElemek.length; i++) {
+            orakElemek[i].style.fontFamily = data.fontstyle;
+        }
+        if (data.darkMode) {
+            root.style.setProperty("--kartyakhatterszine", "black");
+            root.style.setProperty("--szovegszine", "white");
+            root.style.setProperty("--atlatszohattersotetm", "rgba(0, 0, 0, 0.5)");
+            darkModeButton.style.backgroundImage = "url(Sun.png)";
+        } else {
+            root.style.setProperty("--kartyakhatterszine", "white");
+            root.style.setProperty("--szovegszine", "black");
+            root.style.setProperty("--atlatszohattersotetm", "rgba(255, 255, 255, 0.5)");
+            darkModeButton.style.backgroundImage = "url(cartoon-moon-icon.png)";
+        }
+    }
+
+    function loalStorageEllenorzes() {
+        const storedData = localStorage.getItem("projectData");
+        if (storedData) {
+            const parsedData = JSON.parse(storedData);
+            Object.assign(data, parsedData);
+        }
+    }
+
+    function adatHozzaadas() {
+        const selectedColor = favcolorInput.value;
+        data.szovegszine = selectedColor;
+    }
+
+    function adatTorle() {
+        data.szovegszine = "black";
+        data.fontstyle = "Arial";
+    }
+
+    function blackWhiteModValtas() {
+        data.darkMode = !data.darkMode;
+        tartalomRendereles();
+    }
+
+    function blackWhiteModMentesALocalStoragebe() {
+        localStorage.setItem("projectData", JSON.stringify(data));
+    }
+
+    function blackWhiteModBeallitasALocalStoragebol() {
+        loalStorageEllenorzes();
+    }
+
+    function localStorageInicializalas() {
+        blackWhiteModBeallitasALocalStoragebol();
+        tartalomRendereles();
+    }
+
+    menuIcon.addEventListener("click", function () {
+        menu.style.display = "block";
+        cim.style.display = "none";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        menu.style.display = "none";
+        cim.style.display = "block";
+    });
+
+    darkModeButton.addEventListener("click", function () {
+        blackWhiteModValtas();
+        blackWhiteModMentesALocalStoragebe();
+    });
+
+    localStorageInicializalas();
+});
